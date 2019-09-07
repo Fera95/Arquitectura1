a5d8211baa75ef6ca1864bf7d7c8c6f8e63eefb7
crisrivlop <crisrivlop@gmail.com>
2019-09-07 13:44:53 -0600

    modificado:     isa/EXE/KernelSharpen.sv

--- isa/EXE/KernelSharpen.sv
+++ isa/EXE/KernelSharpen.sv
@@ -10,7 +10,7 @@ module KernelSharpen
 	  [ 0,-1, 0]
 	*/
 	logic  [15:0] by5,addition;
-	assign by5 = c[4] <<2 + c[4];
+	assign by5 = (c[4] << 16'd2) + c[4];
 	assign addition = by5 - c[3] - c[5] - c[7] -c[1];
 	assign kadd_res = (addition>16'hFF)? 16'hFF:addition;
 	
